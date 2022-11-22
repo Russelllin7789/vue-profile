8a6550775c7b60c39b43588626e7ea8f449813a4
Russell Lin <superbr7789@gmail.com>
2022-11-22 21:44:00 +0800

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
+    <header class="header">
+      <Navbar />
+    </header>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
 import "./assets/base.css";
-import TheIcon from "./components/TheIcon.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  height: 80px;
+  box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.08);
+}
+
+.footer {
+  text-align: center;
+  padding: 38px 0;
+  color: #828282;
+}
+.footer.inside {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
