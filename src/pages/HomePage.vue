a8830cef6ab9dbf312695bfa8bd7b9709a260584
Russell Lin <superbr7789@gmail.com>
2022-11-26 21:38:22 +0800

    pull out the postList and postItem as universal components

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,88 +1,18 @@
 <template>
   <div>
-    <div class="postList">
-      <div class="postItem" v-for="n in 10">
-        <img
-          src=""
-          alt=""
-          width="100%"
-          height="100%"
-          style="background: #eee"
-        />
-        <div class="postInfo">
-          <div class="postMeta">
-            <TheAvatar />
-            <span>許語彤</span>
-            <span class="postPubDate">12小時之前</span>
-            <PostActions />
-            <div class="postDesc">
-              <p>這是一棵樹，這是一棵樹喔！#植物 #職務</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostList>
+      <PostItem v-for="n in 10" />
+    </PostList>
     <!-- <PostDetails /> -->
     <PostUpload />
   </div>
 </template>
 
 <script setup lang="ts">
-import TheAvatar from "../components/TheAvatar.vue";
 import PostDetails from "../components/PostDetails.vue";
-import PostActions from "../components/PostActions.vue";
+import PostList from "../components/PostList.vue";
+import PostItem from "../components/PostItem.vue";
 import PostUpload from "../components/PostUpload.vue";
 </script>
 
-<style scoped>
-.postList {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 48px;
-}
-
-.postItem {
-  box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
-  border-radius: 8px;
-}
-
-.postInfo {
-  padding: 24px;
-}
-
-.postItem > img {
-  width: 100%;
-  height: 400px;
-  object-fit: cover;
-  background: #eee;
-  cursor: pointer;
-}
-
-.postMeta {
-  display: grid;
-  grid-template-areas:
-    "avatar name actions"
-    "pubDate pubDate actions";
-  grid-template-columns: 42px 1fr 3fr;
-  row-gap: 6px;
-}
-.postMeta .avatar {
-  grid-area: avatar;
-}
-
-.postMeta .postPubDate {
-  grid-area: pubDate;
-  color: #9f9f9f;
-  font-size: 14px;
-}
-
-.postActions {
-  grid-area: actions;
-  justify-self: end;
-}
-
-.postDesc {
-  margin-top: 28px;
-  white-space: pre-line;
-}
-</style>
+<style scoped></style>
